6d56680e989212ca500063ac02af82c4931d52b4
Huy <giahuy20092004@gmail.com>
2025-02-11 10:29:23 +0700

    Update App.js

--- MyExercises/App.js
+++ MyExercises/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import LoginScreen from './src/Bai1BasicComp'; 
 import Component from './src/Bai2_1CustomComp';
 import NotificationList from './src/Bai2_2Flatlist';
+import Lesson5 from './src/Lesson5'
 
 const App = () => {
   // return <LoginScreen />;
   // return <Component />;
-  return <NotificationList />;
+  // return <NotificationList />;
+  return <Lesson5 />;
 };
 
 export default App;
